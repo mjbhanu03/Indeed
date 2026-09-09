c9fea444a9cfda6b7d559760b1f0f157f5f8313d
Mange Jay <jaymange263@gmail.com>
2026-09-09 18:18:16 +0530

    testing

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -3,8 +3,12 @@ const env = require("dotenv")
 env.config()
 module.exports = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  ssl:{
+    rejectUnauthorized: false
+  }
 })
 
